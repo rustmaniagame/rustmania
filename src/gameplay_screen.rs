98d6a5383b3b613bca3c3bc7bf3cc0fef13a8893
RGates94 <rgates275@gmail.com>
2018-08-01 21:53:42 -0400

    refactor to allow for multiple notefields at once

--- src/gameplay_screen.rs
+++ src/gameplay_screen.rs
@@ -7,16 +7,21 @@ use std::time::{Instant, Duration};
 use super::player_config;
 
 pub struct GameplayScreen {
+    notefield: Notefield,
+    p2notefield: Notefield,
+}
+
+pub struct Notefield {
     layout: super::player_config::NoteLayout,
     notes: [Vec<Duration>; 4],
     start_time: Option<Instant>,
 }
 
-impl GameplayScreen {
-    pub fn new(layout: player_config::NoteLayout) -> Self {
-        GameplayScreen {
+impl Notefield {
+    pub fn new(layout: player_config::NoteLayout, notes: [Vec<Duration>; 4]) -> Self {
+        Notefield {
             layout,
-            notes: [Vec::new(), Vec::new(), Vec::new(), Vec::new()],
+            notes,
             start_time: None,
         }
     }
@@ -26,21 +31,7 @@ impl GameplayScreen {
             column.sort();
         }
     }
-    pub fn start(&mut self) {
-        self.start_time = Some(Instant::now());
-    }
-}
-
-fn to_milliseconds(dur: Duration) -> i64 {
-    dur.as_secs() as i64 * 1000 + dur.subsec_millis() as i64
-}
-
-impl ggez::event::EventHandler for GameplayScreen {
-    fn update(&mut self, _ctx: &mut ggez::Context) -> Result<(), ggez::GameError> {
-        Ok(())
-    }
-    fn draw(&mut self, ctx: &mut ggez::Context) -> Result<(), ggez::GameError> {
-        graphics::clear(ctx);
+    fn draw_field(&mut self, ctx: &mut ggez::Context) -> Result<(), ggez::GameError> {
         for &column_position in self.layout.column_positions.iter() {
             graphics::draw(ctx, &self.layout.receptor_sprite, graphics::Point2::new(column_position as f32, self.layout.receptor_height as f32), 0.0)?;
         }
@@ -51,11 +42,40 @@ impl ggez::event::EventHandler for GameplayScreen {
         for (column_index, column_data) in self.notes.iter().enumerate() {
             for note in column_data.iter() {
                 let distance = to_milliseconds(*note) - to_milliseconds(current_time.duration_since(self.start_time.unwrap()));
-                    let position = (distance as f32 * self.layout.scroll_speed) as i64 + self.layout.receptor_height;
+                let position = (distance as f32 * self.layout.scroll_speed) as i64 + self.layout.receptor_height;
                 //let note_graphic = &graphics::Image::solid(ctx, 32, graphics::Color::from_rgb(128,128,128)).unwrap();
                 graphics::draw(ctx, &self.layout.arrow_sprite, graphics::Point2::new(self.layout.column_positions[column_index] as f32, position as f32), 0.0)?;
             }
         }
+        Ok(())
+    }
+}
+
+fn to_milliseconds(dur: Duration) -> i64 {
+    dur.as_secs() as i64 * 1000 + dur.subsec_millis() as i64
+}
+
+impl GameplayScreen {
+    pub fn new(layout: player_config::NoteLayout, notes: [Vec<Duration>; 4], p2layout: player_config::NoteLayout, p2notes: [Vec<Duration>; 4]) -> Self {
+        GameplayScreen {
+            notefield: Notefield::new(layout, notes),
+            p2notefield: Notefield::new(p2layout, p2notes),
+        }
+    }
+    pub fn start(&mut self) {
+        self.notefield.start_time = Some(Instant::now());
+        self.p2notefield.start_time = Some(Instant::now());
+    }
+}
+
+impl ggez::event::EventHandler for GameplayScreen {
+    fn update(&mut self, _ctx: &mut ggez::Context) -> Result<(), ggez::GameError> {
+        Ok(())
+    }
+    fn draw(&mut self, ctx: &mut ggez::Context) -> Result<(), ggez::GameError> {
+        graphics::clear(ctx);
+        self.notefield.draw_field(ctx)?;
+        self.p2notefield.draw_field(ctx)?;
         graphics::present(ctx);
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,33 @@ fn main() {
     let c = conf::Conf::from_toml_file(&mut File::open("src/config.toml").unwrap()).unwrap();
     let context = &mut ggez::Context::load_from_conf("rhythm attempt", "ixsetf", c).unwrap();
 
-    let mut default_layout = player_config::NoteLayout::new([272, 336, 400, 464], ggez::graphics::Image::new(context, "/arrow.png").unwrap(),
+    let mut p1_layout = player_config::NoteLayout::new([72, 136, 200, 264], ggez::graphics::Image::new(context, "/arrow.png").unwrap(),
                                                         ggez::graphics::Image::new(context, "/receptor.png").unwrap(), 50);
 
-    if let Err(e) = default_layout.set_scroll_speed(0.2){
+    let mut p2_layout = player_config::NoteLayout::new([472, 536, 600, 664], ggez::graphics::Image::new(context, "/arrow.png").unwrap(),
+                                                       ggez::graphics::Image::new(context, "/receptor.png").unwrap(), 50);
+
+    if let Err(e) = p1_layout.set_scroll_speed(0.7){
         println!("Couldn't set scroll speed: {}", e);
     }
 
-    let mut game_screen = gameplay_screen::GameplayScreen::new(default_layout);
+    if let Err(e) = p2_layout.set_scroll_speed(1.4){
+        println!("Couldn't set scroll speed: {}", e);
+    }
+
+    let notes = [vec![Duration::from_millis(2000),Duration::from_millis(2375),Duration::from_millis(2750),Duration::from_millis(3000)],
+        vec![Duration::from_millis(2000),Duration::from_millis(2250),Duration::from_millis(2625),Duration::from_millis(3000)],
+        vec![Duration::from_millis(2125),Duration::from_millis(2500),Duration::from_millis(2750),Duration::from_millis(4000)],
+        vec![Duration::from_millis(2250),Duration::from_millis(2500),Duration::from_millis(2875),Duration::from_millis(4000)],
+    ];
+
+    let notes2 = [vec![Duration::from_millis(2000),Duration::from_millis(2375),Duration::from_millis(2750),Duration::from_millis(3000)],
+        vec![Duration::from_millis(2000),Duration::from_millis(2250),Duration::from_millis(2625),Duration::from_millis(3000)],
+        vec![Duration::from_millis(2125),Duration::from_millis(2500),Duration::from_millis(2750),Duration::from_millis(4000)],
+        vec![Duration::from_millis(2250),Duration::from_millis(2500),Duration::from_millis(2875),Duration::from_millis(4000)],
+    ];
+
+    let mut game_screen = gameplay_screen::GameplayScreen::new(p1_layout, notes, p2_layout, notes2);
 
     if let Ok(manifest_dir) = std::env::var("CARGO_MANIFEST_DIR") {
         let mut path = std::path::PathBuf::from(manifest_dir);
@@ -26,11 +45,6 @@ fn main() {
         context.filesystem.mount(&path, true);
     }
 
-    game_screen.add_notes(&mut [vec![Duration::from_millis(2000),Duration::from_millis(2375),Duration::from_millis(2750),Duration::from_millis(3000)],
-        vec![Duration::from_millis(2000),Duration::from_millis(2250),Duration::from_millis(2625),Duration::from_millis(3000)],
-    vec![Duration::from_millis(2125),Duration::from_millis(2500),Duration::from_millis(2750),Duration::from_millis(4000)],
-    vec![Duration::from_millis(2250),Duration::from_millis(2500),Duration::from_millis(2875),Duration::from_millis(4000)],
-                                    ]);
     game_screen.start();
     if let Err(e) = ggez::event::run(context, &mut game_screen) {
         println!("Error: {}", e);
