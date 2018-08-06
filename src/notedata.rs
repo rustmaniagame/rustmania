2c6505fd54054ec3b83249a5e0ac654a010174cd
RGates94 <rgates275@gmail.com>
2018-08-06 18:45:20 -0400

    add basic sm parsing functionality for NoteData

--- src/notedata.rs
+++ src/notedata.rs
@@ -1,11 +1,30 @@
 use std::fs;
 use std::slice;
+use fraction::Fraction;
 
 pub struct TimingData {
     notes: [Vec<i64>; 4],
 }
 
-pub struct _Notedata();
+#[derive(Debug)]
+pub struct NoteData {
+    notes: Vec<Vec<(Fraction, NoteRow)>>,
+}
+
+#[derive(Debug)]
+struct NoteRow {
+    row: Vec<(NoteType,usize)>,
+}
+
+#[derive(Debug)]
+enum NoteType {
+    Tap,
+    Hold,
+    Roll,
+    Mine,
+    Lift,
+    Fake,
+}
 
 impl TimingData {
     pub fn from_sm() -> Self {
@@ -33,5 +52,55 @@ impl TimingData {
     }
     pub fn columns(&self) -> slice::Iter<Vec<i64>> {
         self.notes.iter()
+    }
+}
+
+
+fn parse_measure(measure: &[&str]) -> Vec<(Fraction,NoteRow)> {
+    let mut output = Vec::new();
+    let division = measure.len();
+    for (subindex, beat) in measure.iter().enumerate() {
+        output.push((Fraction::new(subindex as i64,division as u64).unwrap(),parse_line(beat)));
+    }
+    output
+}
+
+fn parse_line(contents: &&str) -> NoteRow {
+    let mut row = Vec::new();
+    contents.chars().enumerate().for_each(|(index, character)| {
+        if let Some(note) = char_to_notetype(character) {
+            row.push((note,index));
+        }
+    });
+    NoteRow {
+        row,
+    }
+}
+
+fn char_to_notetype(character: char) -> Option<NoteType> {
+    match character {
+        '0' => None,
+        '1' => Some(NoteType::Tap),
+        '2' => Some(NoteType::Hold),
+        '4' => Some(NoteType::Roll),
+        'M' => Some(NoteType::Mine),
+        'L' => Some(NoteType::Lift),
+        'F' => Some(NoteType::Fake),
+        _ => None
+    }
+}
+
+impl NoteData {
+    pub fn from_sm() -> Self {
+        let mut notes = Vec::new();
+        let simfile = fs::read_to_string("resources/barebones.sm").unwrap();
+        let lines: Vec<_> = simfile.lines().collect();
+        let measures = lines.split(|&x| x == ",");
+        for measure in measures {
+            notes.push(parse_measure(measure));
+        }
+        NoteData {
+            notes,
+        }
     }
 }
