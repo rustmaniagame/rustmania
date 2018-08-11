974783dee44ea3e3c1be8b8ac90f4d88d943af7a
RGates94 <rgates275@gmail.com>
2018-08-11 17:20:59 -0400

    remove TimingData from_sm function

--- src/notedata.rs
+++ src/notedata.rs
@@ -27,29 +27,6 @@ pub enum NoteType {
 }
 
 impl TimingData {
-    pub fn from_sm() -> Self {
-        let mut notes = [Vec::new(),Vec::new(),Vec::new(),Vec::new()];
-        let bpm = 128;
-        let offset = 1000;
-        let simfile = fs::read_to_string("resources/barebones.sm").unwrap();
-        let lines: Vec<_> = simfile.lines().collect();
-        let measures = lines.split(|&x| x == ",");
-        for (index, measure) in measures.enumerate() {
-            let measure_time = index * 240_000 / bpm;
-            let division = measure.len();
-            for (subindex, beat) in measure.iter().enumerate() {
-                let time = measure_time + (subindex * 240_000 / bpm / division) + offset;
-                for (column, notetype) in beat.chars().enumerate() {
-                    if notetype != '0' {
-                        notes[column].push(time as i64);
-                    }
-                }
-            }
-        }
-        TimingData {
-            notes,
-        }
-    }
     pub fn from_notedata(data: NoteData, bpm: f64, offset: f64) -> Self {
         let mut output = [Vec::new(),Vec::new(),Vec::new(),Vec::new()];
         for ( measure_index , measure) in data.notes.iter().enumerate() {
