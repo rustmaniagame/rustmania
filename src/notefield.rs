rustmaniagame/rustmania

commit 788bfa43fb7d886483465729871176ef112be00b

Simplify logic for adding notes to a batch

src/notefield.rs
@@ -104,21 +104,20 @@ impl<'a> Element for Notefield<'a> {
                 self.layout
                     .add_hold(ctx, column_index, column_data[draw_start].0 - time)?;
             }
-            while draw_end != column_data.len()
+            while draw_end != column_data.len() - 1
                 && (self
                     .layout
                     .delta_to_position(column_data[draw_end].0 - time)
-                    < self.draw_distance || self
-                .layout
-                .delta_to_position(column_data[draw_end].0 - time) > 0)
+                    < self.draw_distance
+                    || self
+                        .layout
+                        .delta_to_position(column_data[draw_end].0 - time)
+                        > 0)
             {
-                if draw_start <= draw_end {
-                    self.layout
-                        .add_note(column_index, &column_data[draw_end..], &mut self.batches);
-                }
                 draw_end += 1;
+                clear_batch = true;
             }
-            while draw_start != column_data.len() && column_data[draw_start].0 - time < -180 {
+            while draw_start != column_data.len() - 1 && column_data[draw_start].0 - time < -180 {
                 self.handle_judgement(None, column_index, column_data[draw_start].2); //this is extremely temporary
                 draw_start += 1;
                 clear_batch = true;

commit fc066eca42fd51a1f282c9882296573983ac3b3b

Fix crash introduced in commit "788bfa43fb7d886483465729871176ef112be00b"

src/notefield.rs
@@ -131,7 +131,7 @@ impl<'a> Element for Notefield<'a> {
                 clear_batch = true;
             }
             let mut next_note = self.column_info[column_index].next_to_hit;
-            while next_note != column_data.len() - 1 && column_data[next_note].0 - time < -180 {
+            while next_note != column_data.len() && column_data[next_note].0 - time < -180 {
                 self.handle_judgement(None, column_index, column_data[next_note].2);
                 next_note += 1;
                 clear_batch = true;