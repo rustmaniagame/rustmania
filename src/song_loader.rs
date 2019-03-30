5515e270748c996d840f66fdf00b4ec46a804d4b
RGates94 <rgates275@gmail.com>
2019-03-30 15:41:10 +0000

    Stop using WalkDir and replace it with std::fs

--- src/song_loader.rs
+++ src/song_loader.rs
@@ -1,11 +1,11 @@
 use crate::notedata;
 use crate::notedata::NoteData;
 use std::ffi::OsStr;
-use std::fs::File;
+use std::fs::{read_dir, File};
 
 pub fn load_song(simfile_folder: &str) -> Option<NoteData> {
-    walkdir::WalkDir::new(simfile_folder.clone())
-        .into_iter()
+    read_dir(simfile_folder)
+        .expect("Couldn't open folder")
         .filter_map(|entry| entry.ok())
         .filter(|entry| entry.path().extension() == Some(OsStr::new("sm")))
         .filter_map(|sim| File::open(sim.path()).ok())
