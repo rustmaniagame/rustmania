b26ab720cb8593051ac56698bbcc6733b20b4048
RGates94 <rgates275@gmail.com>
2019-08-31 03:09:10 +0100

    Remove unused code from clear_screen example

--- graphics/examples/clear_screen.rs
+++ graphics/examples/clear_screen.rs
@@ -4,11 +4,6 @@ use winit::{Event, WindowEvent};
 fn main() {
     let mut winit_state = WinitState::default();
     let mut context = Context::build(&winit_state.window, "Rustmania").unwrap();
-    let (_frame_width, _frame_height) = winit_state
-        .window
-        .get_inner_size()
-        .map(|logical| logical.into())
-        .unwrap_or((0.0, 0.0));
     let mut running = true;
     while running {
         winit_state.events_loop.poll_events(|event| match event {
