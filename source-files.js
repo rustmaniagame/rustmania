fac0f11d8b4d6075ec56300970f6fb7391d84eba
RGates94 <RGates94@users.noreply.github.com>
2019-12-27 06:25:28 +0000

    Automated deployment: Fri Dec 27 06:25:28 UTC 2019 41dac50345b768e3f209f73877e3d150be0e24e9

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["graphics"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["notedata"] = {"name":"","files":["dwi_parser.rs","lib.rs","parser_generic.rs","sm_parser.rs"]};
+sourcesIndex["notedata"] = {"name":"","files":["dwi_parser.rs","lib.rs","parser_generic.rs","sm_parser.rs","sm_writer.rs"]};
 sourcesIndex["rustmania"] = {"name":"","files":["difficulty_calc.rs","gamestate.rs","main.rs","music.rs","notefield.rs","player_config.rs","screen.rs","song_loader.rs","text.rs","timingdata.rs"]};
 createSourceSidebar();
