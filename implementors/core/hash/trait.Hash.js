6a9a1f468c907de012123f6fa47377928bf40fce
RGates94 <RGates94@users.noreply.github.com>
2020-01-16 03:46:43 +0000

    Automated deployment: Thu Jan 16 03:46:43 UTC 2020 bd9832fa63a39bd2db5313ed3c9f7f504eb72a9a

---
+++ implementors/core/hash/trait.Hash.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["notedata"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"notedata/struct.BeatPair.html\" title=\"struct notedata::BeatPair\">BeatPair</a>&lt;T&gt;",synthetic:false,types:["notedata::BeatPair"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"notedata/enum.NoteType.html\" title=\"enum notedata::NoteType\">NoteType</a>",synthetic:false,types:["notedata::NoteType"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"notedata/struct.Note.html\" title=\"struct notedata::Note\">Note</a>",synthetic:false,types:["notedata::Note"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
