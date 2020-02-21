c37100e68dfcacb72c0e892437e45ebac1e45c93
RGates94 <RGates94@users.noreply.github.com>
2020-02-21 03:44:02 +0000

    Automated deployment: Fri Feb 21 03:44:02 UTC 2020 a21086918c5a21db7f48c30e17cc179c17c92776

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["rustmania"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"enum\" href=\"rustmania/screen/enum.ElementType.html\" title=\"enum rustmania::screen::ElementType\">ElementType</a>&gt; for <a class=\"enum\" href=\"rustmania/screen/enum.ElementType.html\" title=\"enum rustmania::screen::ElementType\">ElementType</a>","synthetic":false,"types":["rustmania::screen::ElementType"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
