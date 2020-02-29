eb2c15c9657887104b64680830b872415e3732f1
RGates94 <rgates275@gmail.com>
2020-02-29 18:34:02 -0500

    Add jack scaling to difficulty_calc.rs

--- src/difficulty_calc.rs
+++ src/difficulty_calc.rs
@@ -50,5 +50,43 @@ pub fn rate_chart(notes: &TimingData<CalcInfo>, target: f64) -> f64 {
         }
     }
     //Scale to approximate mina's ratings
-    lower * 3.0
+    lower * 3.15 * jack_scaler(notes)
+}
+
+pub fn jack_scaler(notes: &TimingData<CalcInfo>) -> f64 {
+    let mut out = 0.0;
+    for column in &notes.notes {
+        let mut start_iter = column.notes.iter();
+        while let Some(start_note) = start_iter.next() {
+            let mut total_intermediate = 0;
+            let mut has_missed = false;
+            for end_note in start_iter.clone() {
+                let mut middle_iter = start_iter.clone();
+                //check if notes between start and end can be hit as a jack
+                for i in 0..total_intermediate {
+                    if let Some(middle_note) = middle_iter.next() {
+                        //check if the middle note would be a CB
+                        if start_note.0
+                            + (i + 1) * (end_note.0 - start_note.0) / (total_intermediate + 1)
+                            - middle_note.0.abs()
+                            > 90
+                        {
+                            has_missed = true;
+                            break;
+                        };
+                    }
+                }
+                if has_missed {
+                    break;
+                }
+                total_intermediate += 1;
+            }
+            out += 0.98_f64.powf(total_intermediate as f64);
+        }
+    }
+    out / (notes
+        .notes
+        .iter()
+        .map(|column| column.notes.len())
+        .sum::<usize>()) as f64
 }
